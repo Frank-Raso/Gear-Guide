2782c9cac79531ea17d8a7bd4570fc3daa897909
Frank Raso <frankraso91@gmail.com>
2022-09-29 17:41:03 -0400

    Cleanup

--- src/components/Gear/Gear.jsx
+++ src/components/Gear/Gear.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useHistory, useParams } from "react-router-dom";
-import { Button } from "@material-ui/core";
-import "./Gear.css";
-import { Avatar } from "@material-ui/core";
-import { useState } from "react";
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom';
+import { Button } from '@material-ui/core';
+import './Gear.css';
+import { Avatar } from '@material-ui/core';
+import { useState } from 'react';
 
 function Gear() {
   const user = useSelector((store) => store.user);
@@ -13,11 +13,11 @@ function Gear() {
   const allGear = useSelector((store) => store.allGear);
   const [gear, setGear] = useState(undefined);
   let { id } = useParams();
-  
+
   useEffect(() => {
     console.log(id);
-    dispatch({ type: "GEAR_CONT", payload: id });
-    dispatch({ type: "FETCH_ALL_GEAR" });
+    dispatch({ type: 'GEAR_CONT', payload: id });
+    dispatch({ type: 'FETCH_ALL_GEAR' });
   }, []);
 
   useEffect(() => {
@@ -27,50 +27,54 @@ function Gear() {
         break;
       }
     }
-  },[allGear]);
+  }, [allGear]);
 
-  const delete_Gear = async () => {
-    if (window.confirm("Are you sure you want to delete this gear?")) {
-      dispatch({ type: "DELETE_GEAR", payload: id });
-      setTimeout(function () {
-        history.push("/");
-      }, 1);
-    }
-  };
   const minusOne = () => {
     for (let i = 0; i < allGear.length; i++) {
       if (allGear[i].id == gear.id && i == 0) {
         // we're at the first element, go home instead
-        history.push("/")
-      } else if(allGear[i].id == gear.id) {
+        history.push('/');
+      } else if (allGear[i].id == gear.id) {
         // we're somewhere in the middle
-        history.push(`/gear/${allGear[i-1].id}`);
+        history.push(`/gear/${allGear[i - 1].id}`);
       }
     }
   };
+
   const plusOne = () => {
     for (let i = 0; i < allGear.length; i++) {
       if (allGear[i].id == gear.id && i == allGear.length - 1) {
         // we're at the last element, go home instead
-        history.push("/")
-      } else if(allGear[i].id == gear.id) {
+        history.push('/');
+      } else if (allGear[i].id == gear.id) {
         // we're somewhere in the middle
-        history.push(`/gear/${allGear[i+1].id}`);
+        history.push(`/gear/${allGear[i + 1].id}`);
       }
     }
   };
+
   const whereTheSidewalkEnds = () => {
-    history.push("/profile");
+    history.push('/profile');
   };
+
+  const delete_Gear = async () => {
+    if (window.confirm('Are you sure you want to delete this gear?')) {
+      dispatch({ type: 'DELETE_GEAR', payload: id });
+      setTimeout(function () {
+        history.push('/');
+      }, 1);
+    }
+  };
+
   const editGear = () => {
-    console.log("/In edit");
+    console.log('/In edit');
     history.push(`/edit/${gear.id}`);
   };
 
   const upDel = () => {
-    console.log("upDel");
+    console.log('upDel');
     if (user.id === gear.user_id) {
-      console.log("true");
+      console.log('true');
       return (
         <div>
           <Button variant="contained" color="primary" onClick={delete_Gear}>
@@ -86,7 +90,7 @@ function Gear() {
 
   return (
     <div className="gear_page">
-      {gear == undefined  ? (
+      {gear == undefined ? (
         <div>
           <h1 className="spinner"></h1>
         </div>
