1c6740e96f63ca737e5d2c98b1473459b567cb8f
Frank Raso <frankraso91@gmail.com>
2022-10-18 23:12:56 -0400

    WIP

--- src/components/Collection/Collection.jsx
+++ src/components/Collection/Collection.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory , useParams} from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { useState } from 'react';
 import { Avatar } from '@material-ui/core';
 import { Button } from '@material-ui/core';
@@ -20,40 +20,60 @@ function Collection() {
     history.push(`/gear/${id}`);
   };
 
-  return (
 
+  const followBtn = () => {
+    console.log('followBtn');
+    if(user.id == gear[0].user_id){
+      console.log(`cant follow yourself`);
+    }else{
+      return (
+        <div>
+          <Button variant="contained" color="primary" onClick={followAction} >
+            Follow Me
+          </Button>
+        </div>
+      );
+    }
+
+  }
+const followAction = () => {
+  console.log(`you are now following ${gear[0].user_name}`);
+}
+
+  return (
     <div className="container">
-        {gear == 0 ? (
+      {gear == 0 ? (
         <div>
           <h1 className="spinner"></h1>
         </div>
       ) : (
         <div>
-      <h2> {gear[0].user_name}s' Collection</h2>
-      <div className="AllGearList">
-        {gear.map((eachGear) => {
-          return (
-            <div key={eachGear.id}>
-              <div>
-                <img
-                  className="AllImg"
-                  onClick={() => gearR(eachGear.id)}
-                  src={eachGear.image}
-                ></img>
+          <h2> {gear[0].user_name}s' Collection</h2>
+          <p>{followBtn()}</p>
+          <div className="AllGearList">
+            {gear.map((eachGear) => {
+              return (
+                <div key={eachGear.id}>
+                  <div>
+                    <img
+                      className="AllImg"
+                      onClick={() => gearR(eachGear.id)}
+                      src={eachGear.image}
+                    ></img>
 
-                <h3
-                  onClick={() => gearR(eachGear.id)}
-                  className="AllCatalogTitle"
-                >
-                  {eachGear.year} : {eachGear.title}
-                </h3>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      </div>
-)}
+                    <h3
+                      onClick={() => gearR(eachGear.id)}
+                      className="AllCatalogTitle"
+                    >
+                      {eachGear.year} : {eachGear.title}
+                    </h3>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
