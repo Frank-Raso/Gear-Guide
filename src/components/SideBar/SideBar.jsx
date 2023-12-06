856d3080522dfe026d8244983419f5337e8878e6
Frank Raso <frankraso91@gmail.com>
2023-12-06 16:50:17 -0500

    Condtionally Render SideBar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { SidebarData } from './SidebarData';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
-import { Link } from 'react-router-dom';
-import { SidebarData } from './SidebarData';
-import './SideBar.css';
+import './SideBar.css'; // Adjust the path as necessary
 import { IconContext } from 'react-icons';
 
 function SideBar() {
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => setSidebar(!sidebar);
+
+  // Get the user object from Redux store
+  const user = useSelector((store) => store.user);
+
   return (
     <>
       <IconContext.Provider value={{ color: '#fff' }}>
@@ -21,15 +26,20 @@ function SideBar() {
           <ul className="nav-menu-items" onClick={showSidebar}>
             <li className="navbar-toggle">
               <Link to="#" className="menu-bars">
-                <FaIcons.FaAngleDoubleLeft />
+                <AiIcons.AiOutlineClose />
               </Link>
             </li>
             {SidebarData.map((item, index) => {
+              // Adjust these conditions based on your specific requirements
+              if (!user.id && (item.title === 'Guitars' || item.title === 'Amps' || item.title === 'Accessories' || item.title === 'Following' || item.title === 'Liked' || item.title === 'Add Gear' || item.title === 'All Gear')) {
+                return null; // Don't render these items for non-logged in users
+              }
+
               return (
                 <li key={index} className={item.cName}>
                   <Link to={item.path}>
                     {item.icon}
-                    <span className='spacer'>{item.title}</span>
+                    <span className='spacer' >{item.title}</span>
                   </Link>
                 </li>
               );
